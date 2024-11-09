d81a4dd5eba142d800c6ba3f5ff43e38749e9f1c
Juan Castillo <castillojuanpablo123@gmail.com>
2024-11-08 21:02:01 -0500

    x

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,25 @@
 package com.example;
+import com.example.entidad.empleado;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("miUnidadPersistencia");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        empleado empleado = new empleado();
+        empleado.setNombre("Juan");
+        empleado.setEdad(30);
+
+        em.persist(empleado);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
     }
 }
